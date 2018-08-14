bf0fbd44fc88884f4cedc9eb7ebf455c9e84c9bf
Rafael Queiroz Gonçalves <rafael.q.g@hotmail.com>
2018-08-13 21:49:27 -0300

    implemented function to handle member_of output

--- modules/ldap_extended/do_ldap_memberof_based.php
+++ modules/ldap_extended/do_ldap_memberof_based.php
@@ -18,17 +18,50 @@ echo "</pre><br /><hr /><br />";
 	foreach($users as $user){
 		//missing step to clear all user roles that are LDAP binded.
 		echo "<br />Processing user: ". $user."<br/>";
-		$groups=$ldapExt->get_groups($user);
+		$ldap_search_output=$ldapExt->get_groups($user); 
+		//$TEST_ldap_search_output=array ( 0 => array ("memberof" => array ( 0 => "cn=palo_it_admin,ou=Groups,dc=debortoli,dc=private",1 => "cn=DP_it,ou=Groups,dc=debortoli,dc=private",2 => "cn=risk_management,ou=Groups,dc=debortoli,dc=private", 3 => "cn=DP_quality,ou=Groups,dc=debortoli,dc=private" ))); 
+		//print_r($TEST_ldap_search_output);
+		$prefix="DP_";
+		$groups=getUserDPRoles($prefix,$TEST_ldap_search_output);
+	
 		if(sizeof($groups)==0 || !$groups){
 			echo "<br />No group found on LDAP<br/>";
 		}else{
-			echo "Groups: ". print_r($groups);
-			
+			echo "Groups<br />";
+			print_r($groups);
 			foreach($groups as $group){
 				$ldapExt->addRoleToUser($user, $group);
 			}
 		}	
 	}
 die();
+
+
+function getUserDPRoles($prefix,$ldap_search_output){								
+	$member_of_list=$ldap_search_output[0]["memberof"];
+	$roles=array();
+	foreach($member_of_list as $ldap_member_of_string){
+		//echo $ldap_member_of_string;
+		$posCN=strpos($ldap_member_of_string,"cn=");
+		//echo "POS CN:".$posCN;
+		if($posCN!==false){
+			$posComma=strpos($ldap_member_of_string,",",$posCN);
+			//echo "POS Comma:".$posComma;
+			if($posComma!==false){
+				$cn=substr ( $ldap_member_of_string, $posCN+3,$posComma-3);// $posComma
+				//echo "CN:". $cn . "::"; 
+				$posPrefix=strpos($cn,$prefix);
+				if($posPrefix!==false){
+					
+					$dpRoleName=substr ( $cn, $posPrefix, strlen($cn));
+					//echo "CN: " . $dpRoleName;
+					array_push($roles,$dpRoleName);
+				}
+			}
+		}
+	}
+	return $roles;
+	
+}
 
  ?>
