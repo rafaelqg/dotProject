6b195bbfd8fbd484ad3386270bd8f4dffa782fe3
Matt Bell <matt@2pisoftware.com>
2019-03-26 22:19:16 +1100

    Made gantt charts read-only

--- modules/projects/frappegantt.view.php
+++ modules/projects/frappegantt.view.php
@@ -1,12 +1,37 @@
-<svg id="gantt"></svg>
+<?php
+    $random = substr(md5(mt_rand()), 0, 7);
+?>
+<svg id="gantt-<?php echo $random ?>"></svg>
 <script>
-    var tasks = <?php echo $json ?>;
-    var gantt = new Gantt("#gantt", tasks, {
-        on_click: function (task) {
-            var href = "<?php echo $this->taskClickURL ?>";
-            href = href.replace('%id%', task.id);
-            window.location.href = href;
+    (function(){
+        var tasks = function() {
+            return <?php echo $json ?>;
         }
-    });
-    
+        var resetGantt = function() {
+            setTimeout(() => {
+                gantt.clear();
+                gantt.setup_tasks(tasks());
+                gantt.render();
+            }, 100);
+        }
+        var gantt = new Gantt("#gantt-<?php echo $random ?>", tasks(), {
+            on_click: function (task) {
+                var href = "<?php echo $this->taskClickURL ?>";
+                href = href.replace('%id%', task.id);
+                window.location.href = href;
+            },
+            on_date_change: function(task, start, end) {
+                console.log(task, start, end);
+                resetGantt();
+            },
+            on_progress_change: function(task, progress) {
+                console.log(task, progress);
+                resetGantt();
+            },
+            on_view_change: function(mode) {
+                console.log(mode);
+            }
+        });
+        
+    })();
 </script>
